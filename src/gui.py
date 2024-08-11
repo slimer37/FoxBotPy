6a15f742a48867995c865e6e67e0b38f71755017
Alfred <59073967+slimer37@users.noreply.github.com>
2024-08-11 16:45:21 -0700

    prettier gui (fonts, colors)

--- src/gui.py
+++ src/gui.py
@@ -1,10 +1,13 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QTextEdit, QPushButton, QGroupBox, QHBoxLayout, QLabel, QSizePolicy
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QFontDatabase
 import sys
 
 
 def createTextBoxWithClearButton(title: str, sizeFactor: int):
     group = QGroupBox(title)
+    
+    group.setStyleSheet('font-family: "Rowdies"; font-weight: 300')
         
     sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
     sizePolicy.setHorizontalStretch(sizeFactor)
@@ -22,6 +25,30 @@ def createTextBoxWithClearButton(title: str, sizeFactor: int):
     clearButton = QPushButton(text="Clear")
 
     clearButton.clicked.connect(lambda: textbox.clear())
+    
+    textbox.setStyleSheet("""
+QTextEdit {
+    background-color: #2b065c;
+    color: #E1F7F5;
+    border: none;
+    outline: none;
+}
+""")
+    
+    clearButton.setStyleSheet("""
+QPushButton
+{
+    color: #1E0342;
+    font-weight: 300;
+    border: none;
+    background-color: #E1F7F5;
+}
+
+QPushButton:pressed
+{
+    background-color: #c3d9d7;
+}
+""")
 
     box.addWidget(clearButton)
 
@@ -44,10 +71,13 @@ class Window(QWidget):
         self.resize(800, 400)
         self.setWindowTitle('FoxBot by slimer37')
         
+        self.setStyleSheet('background-color: #1E0342; color: #f4ff7f')
+        
         vlayout = QVBoxLayout()
         self.setLayout(vlayout)
         
         label = QLabel("FoxBot Control Panel")
+        label.setStyleSheet('font-family: "Rowdies"; font-weight: 700;')
         vlayout.addWidget(label)
         
         vlayout.addWidget(label)
@@ -65,7 +95,19 @@ class Window(QWidget):
         sys.stdout = CaptureOutput(self.sysbox.insertPlainText)
 
 app = QApplication(sys.argv)
-    
+
+import os
+
+if getattr(sys, 'frozen', False):
+    path = os.path.join(sys._MEIPASS, "Rowdies")
+else:
+    path = "Rowdies"
+
+for weight in ["Regular", "Light", "Bold"]:
+    fontfile = f"Rowdies-{weight}.ttf"
+    QFontDatabase.addApplicationFont(os.path.join(path, fontfile))
+    print(f"Loaded {fontfile}")
+
 def getwindow():
     window = Window()
     return window
