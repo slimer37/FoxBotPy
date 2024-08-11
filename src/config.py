c3fa7faac08b98a4b2a5769deedd208fc4faa6c3
Alfred <59073967+slimer37@users.noreply.github.com>
2024-08-11 01:09:43 -0700

    clean up config.py and default config

--- src/config.py
+++ src/config.py
@@ -1,19 +1,24 @@
 import configparser
 
+
 def write_default():
     config = configparser.ConfigParser()
-    config['User'] = { 'TargetChannel': '--Some username--' }
-    config['Client'] = { 'ID': '--The app ID--', 'Secret': 'The app secret' }
-    
+    config['User'] = {}
+    config['User']['TargetChannel'] = 'Some username'
+    config['Client'] = {}
+    config['Client']['ID'] = 'The app ID'
+    config['Client']['Secret'] = 'The app secret'
+
     with open('config.ini', 'w') as config_file:
         config.write(config_file)
 
+
 def read_config():
     config = configparser.ConfigParser()
-    
+
     if len(config.read('config.ini')) == 0:
         write_default()
         print("No config file was found. An empty one has been created.")
         return None
-    
+
     return config
