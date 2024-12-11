cc22a0af26e30e50fbfb52176e4ca78e3f9da0a4
Alfred <59073967+slimer37@users.noreply.github.com>
2024-12-10 17:40:00 -0800

    re-raise exception

--- src/main.py
+++ src/main.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     except Exception:
         print(f'\033[31m{traceback.format_exc()}\033[0m')
         print("An error occurred. Press enter to exit.")
-        input()
+        raise
     finally:
         userdb.save()
         datakeeper.writeAll()
